e55f0d3a4265ba02f5b211bd1323e913aea1a4f6
Mike Bai <lostitle@gmail.com>
2014-07-25 16:28:57 +0800

    cryptdb for supervisor

--- cryptdb/cryptdb.sh
+++ cryptdb/cryptdb.sh
@@ -8,7 +8,7 @@ echo "execute /etc/init.d/cryptdb"
 case "$1" in
   start)
     echo "Starting cryptdb"
-    "$EDBDIR"/bins/proxy-bin/bin/mysql-proxy --plugins=proxy --event-threads=4 --max-open-files=1024 --proxy-lua-script=$EDBDIR/mysqlproxy/wrapper.lua --proxy-address=0.0.0.0:3307 --proxy-backend-addresses=127.0.0.1:3306
+    "$EDBDIR"/bins/proxy-bin/bin/mysql-proxy --plugins=proxy --event-threads=4 --max-open-files=1024 --proxy-lua-script=$EDBDIR/mysqlproxy/wrapper.lua --proxy-address=0.0.0.0:3307 --proxy-backend-addresses=127.0.0.1:3306 >> /opt/cryptdb.out 2>&1 &
 #ps aux | grep 'mysql-proxy'
     ;;
   stop)
